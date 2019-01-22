31e7cb15d5fd01c442b81f7f1508f595ffa6b4a1
ghada <ghadakhaled16@gmail.com>
2019-01-22 23:25:41 +0200

    Third screen HomeClinic

---
+++ app/src/main/java/com/example/ghada/newdesigndentelclinic/Homeclinic.java
@@ -0,0 +1,14 @@
+package com.example.ghada.newdesigndentelclinic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Homeclinic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homeclinic);
+    }
+}
---
+++ app/src/main/java/com/example/ghada/newdesigndentelclinic/Signin.java
@@ -0,0 +1,13 @@
+package com.example.ghada.newdesigndentelclinic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+    }
+}
